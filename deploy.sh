39d61faa5be0862819293c9ec802e980aaa2841d
JJ Crosskey <crosskey.jj@gmail.com>
2017-07-11 21:29:51 -0400

    Add option to deploy locally, instead of only to servers

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@
 if [ $# -lt 1 ];
 then
     echo "server name not specified"
-    exit 0
+    echo "cp to local machine" 
+    local=1
+else
+    local=0
+    server=$1
 fi
 
-server=$1
 echo "start deploying dotfiles to $server"
 
 file_dirs=("bash" "git" "python" "tmux")
@@ -14,13 +17,25 @@ do
     echo -e "\ntransfer $dir files..."
     for f in $(ls $dir); do
         echo $f
-        rsync $dir/$f $server:~/.$f
+        if [ $local == "1" ];
+        then
+            rsync $dir/$f ~/.$f
+        else
+            rsync $dir/$f $server:~/.$f
+        fi
     done
 done
 
 echo -e "\ntransfer vim files..."
 f="vim/vimrc"
 echo $f
-rsync $f $server:~/.vimrc
-rsync -ravz vim/ $server:~/.vim/
-rsync -ravz ipython/ $server:~/.ipython/
+if [ $local == "0" ]
+then
+    rsync $f $server:~/.vimrc
+    rsync -ravz vim/ $server:~/.vim/
+    rsync -ravz ipython/ $server:~/.ipython/
+else
+    rsync $f ~/.vimrc
+    rsync -ravz vim/ ~/.vim/
+    rsync -ravz ipython/ ~/.ipython/
+fi
