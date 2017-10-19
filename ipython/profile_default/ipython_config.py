ce124dfb4fd6b3851c8aa7f700e5505055a08a44
JJ Crosskey <jjcrosskey@quantmt.com>
2017-10-18 22:10:10 -0400

    fix bug in ipython_config

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,4 +1,4 @@
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalIPythonApp.extensions = ['line_profiler', 'autoreload']
 c.TerminalIPythonApp.exec_lines = ['%autoreload 2']
-c.TerminalIPythonApp.exec_lines.append('print("Warning: disable autoreload in ipython_config.py to improve performance".)')
+c.TerminalIPythonApp.exec_lines.append('print("Warning: disable autoreload in ipython_config.py to improve performance.")')
